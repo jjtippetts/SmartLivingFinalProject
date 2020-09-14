c8eb791d8579a01bc296723264d5da56868842b5
Jordan Tippetts <jjtippetts@gmail.com>
2020-09-14 13:05:59 -0700

    Converted form to post entire diet instead of many individual post requests. Modified the function to calculate meal nutrients to use the diet object.

--- src/main/resources/static/js/dietPage.js
+++ src/main/resources/static/js/dietPage.js
@@ -1,13 +1,5 @@
 var counter = 0
 
-var mealDictionary = {}
-
-var mealNutrientTotals = {}
-
-var dietId
-
-var newDiet = {}
-
 var newDietBefore = {}
 
 function updateGraph(){
@@ -16,7 +8,7 @@ function updateGraph(){
     })
 }
 
-// Calculates the sum of all the nutrients in each meal by traversin the DOM
+// Calculates the sum of all the nutrients in each meal by traversing the DOM
 function calculateTotalSum() {
     var totals = $(".sum")
 
@@ -31,21 +23,41 @@ function calculateTotalSum() {
 }
 
 // Calculates the sum of nutrients by traversing the DOM
-function calculateSum(table) {
-    //NEED TO CHANGE TO GET BY CLASS
-    var columns = $(table).find("th")
-    var rows = $(table).find("tbody").children()
-    for (var i = 2; i < columns.length - 1; ++i) {
-        var sum = 0
-        for (var o = 0; o < rows.length - 2; ++o) {
-            var rowItems = rows[o].children
-            sum += parseInt(rowItems[i].textContent)
-        }
-        $(table).find("tr.sum").children().eq(i).text(sum)
-        //document.getElementById("sum").children[i].textContent = sum
-        // $('#sum').children()[i].textContent(sum)
+function calculateSum(listOfFoods) {
+
+    var mealTotals = {
+        calories : 0,
+        carbohydrates : 0,
+        fat : 0,
+        protein : 0
     }
+
+    listOfFoods.forEach(function(food){
+        mealTotals.calories += parseInt(food.calories)
+        mealTotals.carbohydrates += parseInt(food.carbohydrates)
+        mealTotals.fat += parseInt(food.fat)
+        mealTotals.protein += parseInt(food.protein)
+    })
+    return mealTotals
 }
+
+function calculateMealNutrients(mealName){
+    var mealNutrientTotal = $("[data-meal-name='" + mealName +"']").closest('tr').next('tr')
+
+    newDietBefore.meals.forEach(function(meal) {
+        if (meal.name === mealName) {
+            var mealTotals = calculateSum(meal.foods)
+            console.log(mealTotals)
+            console.log(mealNutrientTotal)
+            $(mealNutrientTotal).find('.food-calories').text(mealTotals.calories)
+            $(mealNutrientTotal).find('.food-carbohydrates').text(mealTotals.carbohydrates)
+            $(mealNutrientTotal).find('.food-fat').text(mealTotals.fat)
+            $(mealNutrientTotal).find('.food-protein').text(mealTotals.protein)
+        }
+    })
+}
+
+
 
 // Injected Form to create meal after creating a diet plan
 var addMealTable =
@@ -157,10 +169,10 @@ addFoodBody =
     "  <tr class=\"sum table-info sortDisabled\">\n" +
     "    <td>Total</td>\n" +
     "    <td></td>\n" +
-    "    <td>0</td>\n" +
-    "    <td>0</td>\n" +
-    "    <td>0</td>\n" +
-    "    <td>0</td>\n" +
+    "    <td class='food-calories'>0</td>\n" +
+    "    <td class='food-carbohydrates'>0</td>\n" +
+    "    <td class='food-fat'>0</td>\n" +
+    "    <td class='food-protein'>0</td>\n" +
     "  </tr>\n" +
     "</tbody>\n"
 
@@ -178,7 +190,6 @@ var nutrientTotals = "<tr id='nutrient-totals' class=\"table-info\">\n" +
 var form = document.getElementById("addDietPlan")
 form.addEventListener("submit", function(event){
 
-    //var diet = {};
     var formData = new FormData(form);
 
     event.preventDefault();
@@ -188,39 +199,13 @@ form.addEventListener("submit", function(event){
         newDietBefore[key]=value;
     });
     console.log(newDietBefore)
-    var json = JSON.stringify(newDietBefore);
 
-    $.ajax({
-        type: "POST",
-        url: "/dietplan",
-        data: json,
-        headers: {
-            // Important that content type is used to accept json. jquery .post does not work because content type
-            // can't be specified
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        success: function (results) {
-            const dietPlan = results
-            // Replace Diet Form with the name of the dit
-            $("#create-diet-container").html("<h3>Diet Name: " + results.name + "</h3>")
-            // Add a form to enter in a meal name
-            $("#diet-info").html(addMealTable)
-            dietId = results.id // *** TO BE DELETED??
-            // Add a section for the total nutrients for all meals
-            $("#diet-total-nutrients").html("<table class='table'><thead>" +
-                nurtientHeaders + "</thead><tbody>" + nutrientTotals + "</tbody></table>")
-
-            // add diet info into object. May use the object to calc meal totals and save diet plan
-            var JSONstring = JSON.stringify(results)
-            newDiet['id'] = results.id
-            newDiet['name'] = results.name
-            newDiet['meals'] = null
-        },
-        error: function(data){
-            alert("ERROR" + JSON.stringify(data))
-        }
-    });
+    $("#create-diet-container").html("<h3>Diet Name: " + newDietBefore.name + "</h3>")
+    // Add a form to enter in a meal name
+    $("#diet-info").html(addMealTable)
+    // Add a section for the total nutrients for all meals
+    $("#diet-total-nutrients").html("<table class='table'><thead>" +
+        nurtientHeaders + "</thead><tbody>" + nutrientTotals + "</tbody></table>")
 })
 
 //Create A meal
@@ -242,73 +227,35 @@ $(document).on("submit", "form.create-meal", function (e) {
         newDietBefore['meals'].push(meal)
     }
     console.log(newDietBefore)
-    var json = JSON.stringify(meal);
 
-    $.ajax({
-        type: "PUT",
-        url: "/dietplan/" + dietId,
-        data: json,
-        headers: {
-            // Important that content type is used to accept json. jquery .post does not work because content type
-            // can't be specified
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        success: function (results) {
+    $(formElement).closest("tr").after(nurtientHeaders)
+    // Injects the rows for adding a new food to the meal and the total nutrients for that meal
+    $(formElement).closest("thead").after(addFoodBody)
+    var table = $(formElement).closest("table")
+    // Add a new form for adding a meal below the current Meal table
+    $("#diet-info").append(addMealTable)
+    // Replace the form with the name of the meal
+    $(formElement).closest("tr").html("<th colspan='6'>" + formData.get("name") + "</th>")
+    // Alter the id's of the form, label, and input so that there are not duplicates
+    $(table).find("form").attr('id','addMeal' + (++counter))
+    $(table).find("label").attr("form", "addMeal" + (counter))
+    $(table).find("input").attr("form", "addMeal" + (counter))
 
-            // convert results to JSON Object
-            var JSONstring = JSON.stringify(results)
-            if(newDiet['meals'] == null){
-                newDiet['meals'] = [JSON.parse(JSONstring)]
-            }
-            else {
-                newDiet['meals'].push(JSON.parse(JSONstring))
-            }
-
-            // Injects the nutrient header after the meal name
-            $(formElement).closest("tr").after(nurtientHeaders)
-            // Injects the rows for adding a new food to the meal and the total nutrients for that meal
-            $(formElement).closest("thead").after(addFoodBody)
-            var table = $(formElement).closest("table")
-            // Add a new form for adding a meal below the current Meal table
-            $("#diet-info").append(addMealTable)
-            // Replace the form with the name of the meal
-            $(formElement).closest("tr").html("<th colspan='6'>" + formData.get("name") + "</th>")
-            // Alter the id's of the form, label, and input so that there are not duplicates
-            $(table).find("form").attr('id','addMeal' + (++counter))
-            $(table).find("label").attr("form", "addMeal" + (counter))
-            $(table).find("input").attr("form", "addMeal" + (counter))
-
-            // Store the meal id
-            $(table).find(".meal-id").val(results.id)
-
-            $("#foodName").attr("id", "foodName" + (counter))
-            $("#calories").attr("id", "calories" + (counter))
-            $("#carbohydrates").attr("id", "carbohydrates" + (counter))
-            $("#fat").attr("id", "fat" + (counter))
-            $("#protein").attr("id", "protein" + (counter))
-            $("label[for='foodName']").attr("for", "foodName" + (counter))
-            $("label[for='calories']").attr("for", "calories" + (counter))
-            $("label[for='carbohydrates']").attr("for", "carbohydrates" + (counter))
-            $("label[for='fat']").attr("for", "fat" + (counter))
-            $("label[for='protein']").attr("for", "protein" + (counter))
-            Sortable.create($("#diet-info")[0], {
-                group: "shared",
-            });
-            console.log(results)
-            mealDictionary[results.name] = results.id;
-            mealNutrientTotals[results.name] = {
-                calories : 0,
-                carbohydrates : 0,
-                fat : 0,
-                protein : 0,
-            }
-            $(table).find('form').data('data-meal-name',results.name)
-        },
-        error: function(data){
-            console.log("ERROR" + JSON.stringify(data))
-        }
-    })
+    $("#foodName").attr("id", "foodName" + (counter))
+    $("#calories").attr("id", "calories" + (counter))
+    $("#carbohydrates").attr("id", "carbohydrates" + (counter))
+    $("#fat").attr("id", "fat" + (counter))
+    $("#protein").attr("id", "protein" + (counter))
+    $("label[for='foodName']").attr("for", "foodName" + (counter))
+    $("label[for='calories']").attr("for", "calories" + (counter))
+    $("label[for='carbohydrates']").attr("for", "carbohydrates" + (counter))
+    $("label[for='fat']").attr("for", "fat" + (counter))
+    $("label[for='protein']").attr("for", "protein" + (counter))
+    Sortable.create($("#diet-info")[0], {
+        group: "shared",
+    });
+    $(table).find('form').data('data-meal-name', formData.get("name"))
+    $(table).find('form').attr('data-meal-name', formData.get("name"))
 })
 
 //Adding a food to a Meal
@@ -317,7 +264,6 @@ $(document).on("submit", ".addFoodToDiet", function (e) {
     e.preventDefault();
     var formElement = $(this)
     var formData = new FormData($(this)[0])
-    var mealId = formData.get("meal-id")
     formData.delete("meal-id")
     formData.forEach(function(value,key){
         food[key]=value;
@@ -332,87 +278,48 @@ $(document).on("submit", ".addFoodToDiet", function (e) {
             }
         }
     })
-    console.log(newDietBefore)
 
-    var json = JSON.stringify(food);
 
-    $.ajax({
-        type: "PUT",
-        url: "/meal/" + mealId,
-        data: json,
-        headers: {
-            // Important that content type is used to accept json. jquery .post does not work because content type
-            // can't be specified
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        success: function (results) {
-            var JSONstring = JSON.stringify(results)
-            var JSONobject = JSON.parse(JSONstring)
-            newDiet.meals.forEach(function(meal){
-                if(meal.name === $(formElement).data('data-meal-name')){
-                    if(meal.foods == null){
-                        meal.foods = [JSONobject]
-                    }
-                    else{
-                        meal.foods.push(JSONobject)
-                    }
-                }
-            })
-            var row = $("<tr data-food-name = " + results.name + ">")
-            row.append("<td><button class='btn btn-danger rm-food btn-sm'><i class='fas fa-minus'></i></button></td>")
-            row.append("<td>" + results.name + "</td>");
-            row.append("<td>" + results.calories + "</td>");
-            row.append("<td>" + results.carbohydrates + "</td>");
-            row.append("<td>" + results.fat + "</td>");
-            row.append("<td>" + results.protein + "</td>");
-            $(formElement).closest("tr").before(row);
-            mealNutrientTotals[$(formElement).data('data-meal-name')].calories += results.calories
-            mealNutrientTotals[$(formElement).data('data-meal-name')].carbohydrates += results.carbohydrates
-            mealNutrientTotals[$(formElement).data('data-meal-name')].fat += results.fat
-            mealNutrientTotals[$(formElement).data('data-meal-name')].protein += results.protein
+    var row = $("<tr data-food-name = " + formData.get("name") + ">")
+    row.append("<td><button class='btn btn-danger rm-food btn-sm'><i class='fas fa-minus'></i></button></td>")
+    row.append("<td>" + formData.get("name") + "</td>");
+    row.append("<td>" + formData.get("calories") + "</td>");
+    row.append("<td>" + formData.get("carbohydrates") + "</td>");
+    row.append("<td>" + formData.get("fat") + "</td>");
+    row.append("<td>" + formData.get("protein") + "</td>");
+    $(formElement).closest("tr").before(row);
 
-            calculateSum($(formElement).closest("table"))
-            calculateTotalSum()
-            updateGraph()
-            $(".sortable").each(function(index){
-                Sortable.create($(".sortable")[index], {
-                    group: "foodShared",
-                    filter: ".sortDisabled", //prevents moving
-                    preventOnFilter: false, //Allows input to be clickable
-                    onMove: function (evt) { //Prevents moving object around
-                        return evt.related.className.indexOf('sortDisabled') === -1;
-                    },
-                    onAdd : function (evt) {
-                        console.log("Moved '" +
-                            $(evt.to).children().eq(evt.newIndex).attr('data-food-name') + "' from: '" +
-                            $(evt.to).find('form').data('data-meal-name')+ "' to: '" +
-                            $(evt.from).find('form').data('data-meal-name'))
+    var mealName = $(formElement).data('data-meal-name')
+    calculateMealNutrients(mealName)
 
-                        console.log(newDiet)
+    calculateTotalSum()
 
-                        calculateSum($(formElement).closest("table"))
-                    },
-                    onRemove : function (evt) {
-                        calculateSum($(formElement).closest("table"))
-                    }
-                });
-            })
+    $(".sortable").each(function(index){
+        Sortable.create($(".sortable")[index], {
+            group: "foodShared",
+            filter: ".sortDisabled", //prevents moving
+            preventOnFilter: false, //Allows input to be clickable
+            onMove: function (evt) { //Prevents moving object around
+                return evt.related.className.indexOf('sortDisabled') === -1;
+            },
+            onAdd : function (evt) {
+                console.log("Moved '" +
+                    $(evt.to).children().eq(evt.newIndex).attr('data-food-name') + "' from: '" +
+                    $(evt.to).find('form').data('data-meal-name')+ "' to: '" +
+                    $(evt.from).find('form').data('data-meal-name'))
 
-            function swapFoodToMeal(foodName, oldMeal, newMeal, oldIndex, newIndex){
-
+                calculateSum($(formElement).closest("table"))
+            },
+            onRemove : function (evt) {
+                calculateSum($(formElement).closest("table"))
             }
-        },
-        error: function (data) {
-            //alert("Error adding food");
-            console.log(data)
-        },
-    });
+        });
+    })
 });
 
 $(document).on('click', '.rm-food', function (event) {
     var table = $(this).closest("table")
-    var foodToDelete = $(this).parent().parent().remove()
+    $(this).parent().parent().remove()
     calculateSum(table)
     calculateTotalSum()
 })
