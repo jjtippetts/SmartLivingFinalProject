329840e66dda0ff2451aa5c5111f39387b9d4875
Jordan Tippetts <jjtippetts@gmail.com>
2020-11-07 14:06:13 -0800

    changed WebSecurityConfig to include oauth2login. Not functional yet.

--- src/main/java/com/smartliving/webapp/WebSecurityConfig.java
+++ src/main/java/com/smartliving/webapp/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.smartliving.webapp.userdetails.MyUserDetailsService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,10 +15,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
@@ -26,24 +28,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/css/**").permitAll()
-                .antMatchers("/img/**").permitAll()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/test/**").permitAll()
-                .antMatchers("/static/**").permitAll()
-                .antMatchers("/api/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/home", "/js/**", "css/**", "/img/**", "/h2-console/**",
+                                "/test/**", "/static/**", "/api/**", "/login").permitAll()
+                        .anyRequest().authenticated())
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login()
                 .loginPage("/login")
-                .permitAll()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .permitAll();
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll());
+
         //Remove before production. This enables H2 console access
         http.csrf().disable();
         http.headers().frameOptions().disable();
