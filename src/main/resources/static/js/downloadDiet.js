aaff9865e00690e6587659e8ccae4c415f132ae5
Jordan Tippetts <jjtippetts@gmail.com>
2020-11-18 15:24:37 -0800

    Updated the csv file to include meal and diet totals

--- src/main/resources/static/js/downloadDiet.js
+++ src/main/resources/static/js/downloadDiet.js
@@ -6,6 +6,7 @@ $(document).on('click', '#download-diet', function (e) {
     content.push(dietHeader)
 
     selectedDietPlan['meals'].forEach(meal => {
+        content.push([''])
         content.push(["Meal Name: " + meal['name']])
         foodHeader = ['Name', 'Calories', 'Carbs', 'Fat', 'Protein']
         content.push(foodHeader)
@@ -14,7 +15,12 @@ $(document).on('click', '#download-diet', function (e) {
             foodDetails.push(food['name'],food['calories'], food['carbohydrates'], food['fat'], food['protein'])
             content.push(foodDetails)
         })
+        mealTotals = calculateSumOfNutrients(meal['foods'])
+        content.push(['Total Meal Nutrients',mealTotals.calories, mealTotals.carbohydrates, mealTotals.fat, mealTotals.protein])
     })
+    content.push([''])
+    dietTotals = calculateDietNutrients(selectedDietPlan)
+    content.push(['Total Diet Nutrients',dietTotals.calories, dietTotals.carbohydrates, dietTotals.fat, dietTotals.protein])
 
     let csvContent = content.map(e => e.join(",")).join("\n");
     var blob = new Blob([csvContent], {type: "text/csv;charset=utf-8"});
